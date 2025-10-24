5a233166691d7061e181ba5302d775057fe63a2c
operation254 <njagikl@gmail.com>
2025-10-24 17:30:26 +0300

    Add multiple customer insertions for task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,8 @@
-INSERT INTO Customers (customer_id, customer_name, email, address)
+-- Task 6: Insert multiple customers into the customer table
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
