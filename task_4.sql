4bfdc27dc6f584032996ed312916db10447d0e5d
operation254 <njagikl@gmail.com>
2025-10-24 17:25:50 +0300

    Add INFORMATION_SCHEMA query to describe books table

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,7 @@
-SHOW COLUMNS FROM books;
+-- Task 4: Full description of the 'books' table without using DESCRIBE or EXPLAIN
+USE alx_book_store;
+
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
