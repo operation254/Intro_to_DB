5ea7667d386f6a5346a5187c37927da2bf513f53
operation254 <njagikl@gmail.com>
2025-10-24 17:23:46 +0300

    Add SHOW TABLES to task_3.sql for ALX checker

--- task_3.sql
+++ task_3.sql
@@ -1,11 +1,4 @@
--- Task 3: Create Customers table
+-- Task 3: List all tables created
 USE alx_book_store;
 
-CREATE TABLE IF NOT EXISTS customers (
-  customer_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  first_name  VARCHAR(100) NOT NULL,
-  last_name   VARCHAR(100) NOT NULL,
-  email       VARCHAR(255) NOT NULL UNIQUE,
-  address     VARCHAR(255),
-  PRIMARY KEY (customer_id)
-) ENGINE=InnoDB;
+SHOW TABLES;
